719ae4782c6007445250ac4ef369a12e5c01ecd4
Balaji <balajivijay679@gmail.com>
2025-01-24 13:49:19 +0530

    debounce effect

--- src/MovieCardLarge.jsx
+++ src/MovieCardLarge.jsx
@@ -56,7 +56,7 @@ function MovieCardLarge({title}){
   const debouncedFetchData = debounce(async () => {
     let data = await apiReq(URL);
     setArr(data[0]);
-  }, 1000); // Adjust debounce delay (in milliseconds)
+  }, 2000); // Adjust debounce delay (in milliseconds)
 
   useEffect(() => {
     debouncedFetchData(); // Call the debounced function
